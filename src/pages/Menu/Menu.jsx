78bcf9c8924fc4f84903b3a82970d22085e75244
@ToanG1 <toandinh01675@gmail.com>
2023-06-20 22:24:07 +0700

    demo 2

---
+++ src/pages/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./Menu.scss";
+
+function Menu() {
+  return (
+    <>
+      <div></div>
+      <div></div>
+    </>
+  );
+}
+
+export default Menu;
